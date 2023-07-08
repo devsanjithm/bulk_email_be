880d40494b29616583909262213567de4193c973
praveenkumar0023 <apraveenkumar1245@gmail.com>
2023-07-08 23:27:32 +0530

    adding instance

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -35,6 +35,68 @@ export class CreateJobDTO {
   @IsString()
   @IsNotEmpty()
   public job_id: string;
+
+  @IsString()
+  @IsOptional()
+  public email_subject: string;
+
+  @IsString()
+  @IsOptional()
+  public email_from: string;
+
+  @IsString()
+  @IsOptional()
+  public from_email: string;
+
+  @IsString()
+  @IsOptional()
+  public url_domain: string;
+
+  @IsString()
+  @IsOptional()
+  public return_path: string;
+
+  @IsString()
+  @IsOptional()
+  public test_mail: string;
+
+  @IsString()
+  @IsOptional()
+  public header_type: string;
+
+  @IsString()
+  @IsOptional()
+  public check_mode: string;
+
+  @IsString()
+  @IsOptional()
+  public creative_type: string;
+
+  @IsString()
+  @IsOptional()
+  public check_process: string;
+
+  @IsString()
+  @IsOptional()
+  public mode: string;
+
+  @IsString()
+  @IsOptional()
+  public process: string;
+
+  
+  @IsString()
+  @IsOptional()
+  public check_count: string;
+  
+  @IsString()
+  @IsOptional()
+  public instance: string;
+  
+  @IsString()
+  @IsOptional()
+  public mail_content: string;
+
 }
 
 export class updateUserDTO extends GetDto {}
